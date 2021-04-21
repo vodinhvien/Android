8c1efcf1d677ed8684db25dc25cc62fa16662a30
Brian-Vuong20 <63698642+Brian-Vuong20@users.noreply.github.com>
2021-04-21 14:35:37 +0700

    first commit

--- LocationApp/app/src/main/java/com/example/locationapp/MainActivity.java
+++ LocationApp/app/src/main/java/com/example/locationapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.*;
 
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     public Button btnWebsite;
     public Button btnMap;
+    public Button btnClear;
+
     public EditText edtLocation;
 
 
@@ -24,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
         btnMap = (Button) findViewById(R.id.btnMap);
         btnWebsite = (Button) findViewById(R.id.btnWeb);
+        btnClear = (Button) findViewById(R.id.clearSearch);
+
 
         edtLocation = (EditText) findViewById(R.id.eLocation);
 
@@ -40,26 +45,44 @@ public class MainActivity extends AppCompatActivity {
                 findOnWebsite();
             }
         });
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clear();
+            }
+        });
 
     }
+
+
 
     public void findOnGoogleMap() {
 
         String txt = edtLocation.getText().toString();
-
-        Uri uriGoogleMap = Uri.parse("geo:0,0?q=" + txt);
-        Intent intentGoogleMap = new Intent(Intent.ACTION_VIEW, uriGoogleMap);
-        intentGoogleMap.setPackage("com.google.android.apps.maps");
-        startActivity(intentGoogleMap);
+        if(txt.length() > 0) {
+            Uri uriGoogleMap = Uri.parse("geo:0,0?q=" + txt);
+            Intent intentGoogleMap = new Intent(Intent.ACTION_VIEW, uriGoogleMap);
+            intentGoogleMap.setPackage("com.google.android.apps.maps");
+            startActivity(intentGoogleMap);
+        } else {
+            Toast.makeText(MainActivity.this, "Input field must not be empty", Toast.LENGTH_SHORT).show();
+        }
     }
+
 
     public void findOnWebsite() {
-        String txt = edtLocation.getText().toString();
-
-
-        Intent intentGoogleSearch = new Intent(Intent.ACTION_WEB_SEARCH);
-        intentGoogleSearch.putExtra(SearchManager.QUERY, txt);
-        startActivity(intentGoogleSearch);
-
+            String txt = edtLocation.getText().toString();
+            if(txt.length() > 0) {
+                Intent intentGoogleSearch = new Intent(Intent.ACTION_WEB_SEARCH);
+                intentGoogleSearch.putExtra(SearchManager.QUERY, txt);
+                startActivity(intentGoogleSearch);
+                Log.i("length", String.valueOf(txt.length()));
+            } else {
+                Toast.makeText(MainActivity.this, "Input field must not be empty", Toast.LENGTH_SHORT).show();
+        }
     }
+    public void clear() {
+        edtLocation.setText(" ");
+    }
+
 }
